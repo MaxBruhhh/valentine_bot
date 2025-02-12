e08ce3b27e8b02fb290616b6f2ccda7290b4775a
MaxBruhhh <m.semin812@gmail.com>
2025-02-12 21:08:23 +0300

    Update valentizer.py

--- valentizer.py
+++ valentizer.py
@@ -294,7 +294,7 @@ def main():
     application.run_webhook(
         listen="0.0.0.0",
         port=10000,
-        webhook_url=f"https://valentine-bot-evo4.onrender.com",
+        webhook_url=f"https://valentine-bot-evo4.onrender.com/webhook",
         url_path=TOKEN,
     )
 
